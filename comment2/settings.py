758a09a3996e1b55e02bbb098668592b184618c6
alankwok13 <44396677+alankwok13@users.noreply.github.com>
2019-03-15 15:45:03 +0800

    refresh all files
    refresh for unknown error

--- comment2/settings.py
+++ comment2/settings.py
@@ -74,16 +74,6 @@ WSGI_APPLICATION = 'comment2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': 'movie1',
-#		'USER':'root',
-#		'PASSWORD':'@123@@',
-#		'POST':'3306',
-#    }
-#}
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
