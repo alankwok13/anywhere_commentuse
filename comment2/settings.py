e26fd5b6066ceb2765f04cda6175fd9b6ffdd417
alankwok13 <44396677+alankwok13@users.noreply.github.com>
2019-03-15 14:43:29 +0800

    database
    local database

--- comment2/settings.py
+++ comment2/settings.py
@@ -74,13 +74,20 @@ WSGI_APPLICATION = 'comment2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'movie1',
+#		'USER':'root',
+#		'PASSWORD':'@123@@',
+#		'POST':'3306',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'movie1',
-		'USER':'root',
-		'PASSWORD':'@123@@',
-		'POST':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
